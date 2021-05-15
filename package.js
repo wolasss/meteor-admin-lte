43106b3e3150985721ff90e9beaedb35a7fc267d
Adam Wolski <wolas.abc@gmail.com>
2021-05-15 17:04:40 +0200

    fix: ugprade to admin lte v3

--- package.js
+++ package.js
@@ -1,37 +1,25 @@
 Package.describe({
-  name: 'wolas:admin-lte',
-  version: '0.0.6',
-  summary: 'AdminLTE dashboard theme',
-  git: 'https://github.com/wolasss/meteor-admin-lte',
-  documentation: 'README.md'
+    name: 'wolas:admin-lte-3',
+    version: '0.0.9',
+    summary: 'AdminLTE dashboard theme',
+    git: 'https://github.com/wolasss/meteor-admin-lte',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('2.0');
+    api.versionsFrom('2.2');
 
-  api.use([
-    'templating',
-    'reactive-var'
-  ], 'client');
+    api.use(['templating', 'reactive-var', 'jquery'], 'client');
 
-  api.addFiles([
-    'admin-lte.html',
-    'admin-lte.js'
-  ], 'client');
+    api.addFiles([
+        'admin-lte.html',
+        'Layout.js',
+        'PushMenu.js',
+        'Treeview.js',
+        'Dropdown.js',
+        'admin-lte.js'
+    ],
+    'client');
 
-  api.addFiles([
-    'css/AdminLTE.min.css',
-    'css/skins/skin-black-light.min.css',
-    'css/skins/skin-black.min.css',
-    'css/skins/skin-blue-light.min.css',
-    'css/skins/skin-blue.min.css',
-    'css/skins/skin-green-light.min.css',
-    'css/skins/skin-green.min.css',
-    'css/skins/skin-purple-light.min.css',
-    'css/skins/skin-purple.min.css',
-    'css/skins/skin-red-light.min.css',
-    'css/skins/skin-red.min.css',
-    'css/skins/skin-yellow-light.min.css',
-    'css/skins/skin-yellow.min.css'
-  ], 'client', { isAsset: true });
+    api.addFiles(['css/AdminLTE.min.css'], 'client', { isAsset: true });
 });
